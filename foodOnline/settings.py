9f1759157ec86cd5d28961624ebf4b99c8a4e8dd
Sagar Darade <daradesagar.1998@gmail.com>
2022-11-22 21:51:45 +0530

    create custom user model, Make migrations, Create superuser

--- foodOnline/settings.py
+++ foodOnline/settings.py
@@ -132,3 +132,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custome user model
+AUTH_USER_MODEL = 'accounts.User'
