8531bd13e36a95ed6706e40e94da587a337ffb05
Sagar Darade <daradesagar.1998@gmail.com>
2022-11-22 21:58:20 +0530

    hide password in admin pannel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 from .models import User
 
+class CustomUserAdmin(UserAdmin):
+    list_display = ['email','first_name','last_name','username','role','is_active']
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+    ordering = ['-date_joined','username']
 
-admin.site.register(User)
+
+admin.site.register(User, CustomUserAdmin)
