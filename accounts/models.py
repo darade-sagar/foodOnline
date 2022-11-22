0a460e0267915044308abd824431f6ba33b034cc
Sagar Darade <daradesagar.1998@gmail.com>
2022-11-22 21:16:43 +0530

    added account app

---
+++ accounts/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+# Create your models here.
+
+
+class UserManager(BaseUserManager):
+    pass
+
+class User(AbstractBaseUser):
+    pass 
