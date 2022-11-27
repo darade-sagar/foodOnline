3593793fb4dda4ca3abdedfafbda9ffd87a36cf8
Sagar Darade <daradesagar.1998@gmail.com>
2022-11-27 17:22:38 +0530

    Vendor Update Profile design and data save into db. All logic

--- vendor/forms.py
+++ vendor/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from .models import Vendor
 
 class VendorForm(forms.ModelForm):
+    vendor_licence = forms.ImageField(widget=forms.FileInput(attrs={'class': 'btn btn-info',}))
+
     class Meta:
         model = Vendor
         fields = ['vendor_name','vendor_licence']
