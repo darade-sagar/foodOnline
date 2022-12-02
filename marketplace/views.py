a324b40f4c96537bbff70035bedb8e78ae80c964
Sagar Darade <daradesagar.1998@gmail.com>
2022-12-02 12:01:24 +0530

    list menu-category for restaurant and load menu of related category by Prefetch

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from vendor.models import Vendor
+from menu.models import Category, FoodItem
+
+from django.db.models import Prefetch
+
 # Create your views here.
 def marketplace(request):
     vendors = Vendor.objects.filter(is_approved=True, user__is_active=True)
@@ -12,7 +16,15 @@ def marketplace(request):
 
 def vendor_detail(request, vendor_slug):
     vendor = get_object_or_404(Vendor,vendor_slug=vendor_slug)
+    categories = Category.objects.filter(vendor=vendor).prefetch_related(
+        Prefetch(
+            'fooditems',
+            queryset= FoodItem.objects.filter(is_available=True)
+        )
+    )
+    
     context ={
         'vendor' : vendor,
+        'categories':categories,
     }
     return render(request,'marketplace/vendor_detail.html',context)
