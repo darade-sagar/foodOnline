03e392088b554d82c55ccc57f39854d1c8d42b8c
Sagar Darade <daradesagar.1998@gmail.com>
2023-01-02 17:22:00 +0530

    error handling during payment

--- orders/views.py
+++ orders/views.py
@@ -116,97 +116,103 @@ def is_ajax(request):
 
 def payments(request):
     # check if request is Ajax
-    if is_ajax(request) and request.method=="POST":
+    try:
+        if is_ajax(request) and request.method=="POST":
 
-        # Store Payment details in Payment Model
-        order_number = request.POST['order_number']
-        transaction_id = request.POST['transaction_id']
-        payment_method = request.POST['payment_method']
-        status = request.POST['status']
-        order = Order.objects.get(user=request.user,order_number=order_number)
-        payment = Payment(
-            user=request.user,
-            transaction_id=transaction_id,
-            payment_method = payment_method,
-            amount = order.total,
-            status = status
-        )
-        payment.save()
+            # Store Payment details in Payment Model
+            order_number = request.POST['order_number']
+            transaction_id = request.POST['transaction_id']
+            payment_method = request.POST['payment_method']
+            status = request.POST['status']
+            order = Order.objects.get(user=request.user,order_number=order_number)
+            payment = Payment(
+                user=request.user,
+                transaction_id=transaction_id,
+                payment_method = payment_method,
+                amount = order.total,
+                status = status
+            )
+            payment.save()
 
-        # Update the order model
-        order.payment = payment
-        order.is_ordered =True
-        order.save()
+            # Update the order model
+            order.payment = payment
+            order.is_ordered =True
+            order.save()
+            
 
-        # move order to old ordered foods model
-        cart_items = Cart.objects.filter(user=request.user)
-        for item in cart_items:
-            ordered_food = OrderedFood()
-            ordered_food.order = order
-            ordered_food.payment = payment
-            ordered_food.user = request.user
-            ordered_food.fooditem = item.fooditem
-            ordered_food.quantity = item.quantity
-            ordered_food.price = item.fooditem.price #type:ignore
-            ordered_food.amount = item.fooditem.price * item.quantity #type:ignore
-            ordered_food.save()
+            # move order to old ordered foods model
+            cart_items = Cart.objects.filter(user=request.user)
+            for item in cart_items:
+                ordered_food = OrderedFood()
+                ordered_food.order = order
+                ordered_food.payment = payment
+                ordered_food.user = request.user
+                ordered_food.fooditem = item.fooditem
+                ordered_food.quantity = item.quantity
+                ordered_food.price = item.fooditem.price #type:ignore
+                ordered_food.amount = item.fooditem.price * item.quantity #type:ignore
+                ordered_food.save()
 
-        # Send Notifications to each vendor
-        mail_subject='You have received new Order | foodOnline'
-        template='orders/new_order_received.html'
-        to_mails = []
-        for i in cart_items:
-            if i.fooditem.vendor.user.email not in to_mails:
-                to_mails.append(i.fooditem.vendor.user.email)
+            # Send Notifications to each vendor
+            mail_subject='You have received new Order | foodOnline'
+            template='orders/new_order_received.html'
+            to_mails = []
+            for i in cart_items:
+                if i.fooditem.vendor.user.email not in to_mails:
+                    to_mails.append(i.fooditem.vendor.user.email)
 
-                ordered_food_to_vendor = OrderedFood.objects.filter(order=order,fooditem__vendor = i.fooditem.vendor)
-                
-                        
-                context={
-                    'order':order,
-                    'to_email':[i.fooditem.vendor.user.email],
-                    'ordered_food_to_vendor':ordered_food_to_vendor,
-                    'vendor':i.fooditem.vendor,
-                    'vendor_subtotal':order_total_by_vendor(order=order,vendor=i.fooditem.vendor)['subtotal'],
-                    'tax_data':order_total_by_vendor(order=order,vendor=i.fooditem.vendor)['tax_dict'],
-                    'vendor_grand_total':order_total_by_vendor(order=order,vendor=i.fooditem.vendor)['grand_total'],
-                }
-                send_notification(mail_subject,template,context)
+                    ordered_food_to_vendor = OrderedFood.objects.filter(order=order,fooditem__vendor = i.fooditem.vendor)
+                    
+                            
+                    context={
+                        'order':order,
+                        'to_email':[i.fooditem.vendor.user.email],
+                        'ordered_food_to_vendor':ordered_food_to_vendor,
+                        'vendor':i.fooditem.vendor,
+                        'vendor_subtotal':order_total_by_vendor(order=order,vendor=i.fooditem.vendor)['subtotal'],
+                        'tax_data':order_total_by_vendor(order=order,vendor=i.fooditem.vendor)['tax_dict'],
+                        'vendor_grand_total':order_total_by_vendor(order=order,vendor=i.fooditem.vendor)['grand_total'],
+                    }
+                    send_notification(mail_subject,template,context)
 
-        # Send Notifications to customer
-        mail_subject='Thank you for Ordering with us | foodOnline'
-        template='orders/order_confirmation.html'
-        to_mails = [order.email]
-        ordered_food = OrderedFood.objects.filter(order=order)
-        customer_subtotal = 0
-        for item in ordered_food:
-            customer_subtotal += (item.price*item.quantity)
-        tax_data = order.tax_data
-        print(tax_data)
-        context={
-            'order':order,
-            'to_email':to_mails,
-            'user':request.user,
-            'ordered_food':ordered_food,
-            'customer_subtotal':customer_subtotal,
-            'tax_data':tax_data,
-        }
-        send_notification(mail_subject,template,context)
+            # Send Notifications to customer
+            mail_subject='Thank you for Ordering with us | foodOnline'
+            template='orders/order_confirmation.html'
+            to_mails = [order.email]
+            ordered_food = OrderedFood.objects.filter(order=order)
+            customer_subtotal = 0
+            for item in ordered_food:
+                customer_subtotal += (item.price*item.quantity)
+            tax_data = order.tax_data
+            print(tax_data)
+            context={
+                'order':order,
+                'to_email':to_mails,
+                'user':request.user,
+                'ordered_food':ordered_food,
+                'customer_subtotal':customer_subtotal,
+                'tax_data':tax_data,
+            }
+            send_notification(mail_subject,template,context)
+            
+            # Clear the cart
+            cart_items.delete()
 
-        # Clear the cart
-        # cart_items.delete()
+            # Clear orders which dont have any payment
+            orders_without_payment = Order.objects.filter(user=request.user,is_ordered=False)
+            for order in orders_without_payment:
+                order.delete()
 
-        # Clear orders which dont have any payment
-        orders_without_payment = Order.objects.filter(user=request.user,is_ordered=False)
-        for order in orders_without_payment:
-            order.delete()
-
-        # redirect order completion page
-        response = {
-            'order_number':order_number,
-            'transaction_id':transaction_id
-        }
-        return JsonResponse(response)
+            # redirect order completion page
+            response = {
+                'order_number':order_number,
+                'transaction_id':transaction_id,
+                'status':'success',
+            }
+            return JsonResponse(response)
+        
+    except:
+        return JsonResponse({'status':'failed'})
     return HttpResponse("PaymentPage")
 
 def order_complete(request):
